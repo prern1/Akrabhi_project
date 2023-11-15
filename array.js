e6f33e0fca46bffd0ba66dd2f41436d5752c459e
Prerna Patra <116371962+prern1@users.noreply.github.com>
2023-11-15 13:55:53 +0530

    Add files via upload

---
+++ array.js
@@ -0,0 +1,7 @@
+function xyz(){
+const cars = ["Saab", "Volvo", "BMW"];
+let car = cars[1];
+document.write(car);
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("demo").innerHTML = fruits.toString();
+}
