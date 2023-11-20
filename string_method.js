bfcf1283e56b8a153f9bbe036020c67ca506b422
Prerna Patra <116371962+prern1@users.noreply.github.com>
2023-11-20 10:03:16 +0530

    Add files via upload

---
+++ string_method.js
@@ -0,0 +1,8 @@
+const name="Prerana"
+const age=23
+console.log(`Hello my name is ${name} and my age is ${age}`);
+
+console.log(name.length);
+console.log(name[2]);
+console.log(name.charAt(4));
+console.log(name.normalise);
